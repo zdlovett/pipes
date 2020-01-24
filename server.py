928bd0afa10f00e418522892e8aa69938e5e8daa
zach lovett <zdlovett@gmail.com>
2020-01-23 23:17:48 -0800

    working on getting login flow wired, need to get the pipes page setup, and add the endpoints for generating and checking the uuids. also need to figure out how and were to store the uuids

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 
 from uuid import uuid4
-from flask import Flask, escape, render_template
+from flask import Flask, escape, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return render_template("start.html")
+def root():
+    return redirect(url_for("start"))
+
+
+@app.route("/start", methods=["GET", "POST"])
+def start():
+    if request.method == "GET":
+        return render_template("start.html")
+    else:
+        return "new session"
 
 
 @app.route("/get_session")
